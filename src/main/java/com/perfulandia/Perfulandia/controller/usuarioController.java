b6e13c44b16a92da90412d41321dd4e73a9c73b9
Flavio Henriquez <fla.henriquez@duocuc.cl>
2025-05-20 15:16:54 -0400

    ...

--- src/main/java/com/perfulandia/Perfulandia/controller/usuarioController.java
+++ src/main/java/com/perfulandia/Perfulandia/controller/usuarioController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.perfulandia.Perfulandia.model.usuario;
-import com.perfulandia.Perfulandia.repository.usuarioRepository;
+
 import com.perfulandia.Perfulandia.service.usuarioService;
 
 @RestController
