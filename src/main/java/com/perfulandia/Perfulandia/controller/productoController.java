fa5d9e65ada351f8430d3a7715568f10de9d1ec4
Flavio Henriquez <fla.henriquez@duocuc.cl>
2025-05-20 15:23:00 -0400

    index

--- src/main/java/com/perfulandia/Perfulandia/controller/productoController.java
+++ src/main/java/com/perfulandia/Perfulandia/controller/productoController.java
@@ -3,11 +3,6 @@ package com.perfulandia.Perfulandia.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.perfulandia.Perfulandia.service.productoService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
